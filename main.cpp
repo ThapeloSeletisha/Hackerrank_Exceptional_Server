6b07d90c452088b481f83cc323a86b81e9e59b37
ThapeloSeletisha <thapeloseletisha01@gmail.com>
2022-07-01 18:48:22 +0200

    handled given exceptions

--- main.cpp
+++ main.cpp
@@ -35,6 +35,22 @@ int main() {
 		cin >> A >> B;
 
 		/* Enter your code here. */
+        try {
+            cout << Server::compute(A, B) << endl;
+        } 
+        catch(invalid_argument e) 
+        {
+            cout << "Exception: "<< e.what() << endl;
+        }
+        catch(bad_alloc e)
+        {
+            cout << "Not enough memory"<< endl;
+        }
+        catch(exception e)
+        {
+            cout << "Other Exception" << endl;
+        }
+
 
 	}
 	cout << Server::getLoad() << endl;
